8b4efd9cf708898efa5ba1352a0b0c9697d36a3f
Carl0017 <smsmartgrowth@gmail.com>
2025-02-12 08:20:25 -0800

    First Commit && Page Setup

---
+++ src/Pages/Home/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.scss'
+import { Music } from 'lucide-react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Router} from 'react-router-dom'
+import './index.scss'
+
+
+//pages
+import App from './Pages/Home/App'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
