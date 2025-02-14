f247a695b85431a656fa5122384c267feb4a934b
Carl0017 <smsmartgrowth@gmail.com>
2025-02-14 02:07:34 -0800

    Search Page Ui Mobile Responsive

--- src/Pages/SpotifySearch/App.jsx
+++ src/Pages/SpotifySearch/App.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import Navbar from "../../Components/Navbar/App"
+import React from "react";
+import Navbar from "../../Components/Navbar/App";
 
-import "./App.scss"
-import { Link } from 'lucide-react'
-
+import "./App.scss";
+import { Link } from "lucide-react";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
       <div className="mainPageWrapper">
-      <div className="searchWrapper">
-        <h2>
-          Spotify Playlist Downloader
-        </h2>
-        <p>
-        Download any public Spotify playlist, convert tracks to MP3, and take your music offline effortlessly!
-        </p>
+        <div className="searchWrapper">
+          <h2>Spotify Playlist Downloader</h2>
+          <p>
+            Download any public Spotify playlist, convert tracks to MP3, and
+            take your music offline effortlessly!
+          </p>
 
-        <div id="searchInput">
+          <div id="searchInput">
             <Link />
-            <input type="text" placeholder='Paste a Spotify playlist URL here' />
-            <button>
-              Search
-            </button>
-        </div>
+            <input
+              type="text"
+              placeholder="Paste a Spotify playlist URL here"
+            />
+            <button>Search</button>
+          </div>
+          
+          <button className="mobileButton">Search</button>
 
-       <div id="downloadConsent">
-         <p>By downloading this Spotify Playlist, you agree to the <span style={{color:"",}}>Usage Guidelines.</span></p>
-       </div>
-     </div>
+          <div id="downloadConsent">
+            <p>
+              By downloading this Spotify Playlist, you agree to the{" "}
+              <span style={{ color: "" }}>Usage Guidelines.</span>
+            </p>
+          </div>
+        </div>
       </div>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
