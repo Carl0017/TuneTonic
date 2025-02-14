58c60b6c5b2dc601cbb1905d4138a425aa657560
Carl0017 <smsmartgrowth@gmail.com>
2025-02-14 01:49:43 -0800

    Search Page Ui

--- src/Pages/SpotifySearch/App.jsx
+++ src/Pages/SpotifySearch/App.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import Navbar from "../../Components/Navbar/App"
 
 import "./App.scss"
+import { Link } from 'lucide-react'
 
 
 function App() {
   return (
-    <div>
-      Search Page
-    </div>
+    <>
+    <Navbar/>
+
+      <div className="mainPageWrapper">
+      <div className="searchWrapper">
+        <h2>
+          Spotify Playlist Downloader
+        </h2>
+        <p>
+        Download any public Spotify playlist, convert tracks to MP3, and take your music offline effortlessly!
+        </p>
+
+        <div id="searchInput">
+            <Link />
+            <input type="text" placeholder='Paste a Spotify playlist URL here' />
+            <button>
+              Search
+            </button>
+        </div>
+
+       <div id="downloadConsent">
+         <p>By downloading this Spotify Playlist, you agree to the <span style={{color:"",}}>Usage Guidelines.</span></p>
+       </div>
+     </div>
+      </div>
+    
+    </>
   )
 }
 
